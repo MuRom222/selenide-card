1a04e208d5f4c69a860d2502ea59ce28664bc36d
Roman Mukovnin <r.mykovnin@gmail.com>
2023-01-09 23:55:06 +0300

    editing

--- src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
@@ -13,7 +13,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class AppCardDeliveryTest {
 
-    private String generateDate (int addDays,String pattern) {
+     String generateDate (int addDays,String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
@@ -23,7 +23,7 @@ public class AppCardDeliveryTest {
 
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Воронеж");
-        String currentDate = generateDate(5,"dd.MM.yyyy");
+        String currentDate = generateDate(7,"dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(currentDate);
         $("[data-test-id='name'] input").setValue("Виктор Белов");
@@ -31,7 +31,7 @@ public class AppCardDeliveryTest {
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(7))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
 
 
